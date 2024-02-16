a6590d6c0e34180e78bac1caa6615f3075520183
Rafael Beltran <rafabeltrans17@gmail.com>
2024-02-16 12:52:51 -0600

    Ejercicios actividad euclides

--- actividad_2/euclides.py
+++ actividad_2/euclides.py
@@ -1,9 +1,13 @@
 def euclides(num_1, num_2):
-    if num_1 == 0:
-        return num_2
-    elif num_2 == 0:
+    if num_2 == 0:
         return num_1
     else:
         return euclides(num_2, num_1 % num_2)
 
-print(euclides(270, 192))
+
+num_1 = int(input("Ingresa el primer número: "))
+num_2 = int(input("Ingresa el segundo número: "))
+
+mcd = euclides(num_1, num_2)
+
+print(f"{num_1} | {num_2}: MCD = {mcd}")
